c3c1b978a4ee95c4508b53a6ce5a5684d4c19c6b
Gamin21 <139269351+Gamin21@users.noreply.github.com>
2025-08-08 21:01:39 +0600

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const decreaseBtn = document.getElementById("decreaseBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
@@ -15,11 +16,11 @@ decreaseBtn.onclick = function() {
     count--;
     countlabel.textContent = count;
   }
-
+}
 
 resetBtn.onclick = function(){
 
 count = 0;
 countlabel.textContent = count;                                                                                                                                        
-
 }
+
